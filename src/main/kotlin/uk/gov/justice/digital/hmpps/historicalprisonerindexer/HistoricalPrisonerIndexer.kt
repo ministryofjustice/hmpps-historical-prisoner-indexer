12373d364573260425a8182143f4ba2f2a469b08
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-09-26 11:13:47 +0100

    HAAR-895: Add sqlserver config and flyway to add hpa stage data

--- src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerindexer/HistoricalPrisonerIndexer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerindexer/HistoricalPrisonerIndexer.kt
@@ -1,18 +1,9 @@
 package uk.gov.justice.digital.hmpps.historicalprisonerindexer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRepositoriesAutoConfiguration
-import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRestClientAutoConfiguration
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-  exclude = [
-    ReactiveElasticsearchRestClientAutoConfiguration::class,
-    ReactiveElasticsearchRepositoriesAutoConfiguration::class
-  ]
-)
-@ConfigurationPropertiesScan
+@SpringBootApplication
 class HistoricalPrisonerIndexer
 
 fun main(args: Array<String>) {
