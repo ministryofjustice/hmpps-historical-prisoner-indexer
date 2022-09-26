6251d3307cca823d4af2260e0fc6e5e0728410bc
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-09-26 11:17:13 +0100

    HAAR-895: update import order

--- src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerindexer/repository/AddressesRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerindexer/repository/AddressesRepository.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.historicalprisonerindexer.repository
 
-import uk.gov.justice.digital.hmpps.historicalprisonerindexer.model.Addresses
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import uk.gov.justice.digital.hmpps.historicalprisonerindexer.model.Addresses
 
 @Repository
 interface AddressesRepository : CrudRepository<Addresses, Long>
